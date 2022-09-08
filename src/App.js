b4c7a15bdc0d04b3df6a523fa7222fd3ea1529e9
dimavoronin <zveruha1109>
2022-09-08 11:14:07 +0300

    added menu npm

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import bg from './assets/background.png';
-import Menu from './components/Menu';
+import Menu from 'airdao-menu/build';
 import { Web3ReactProvider } from '@web3-react/core';
 import {providers} from "ethers";
 import useAutoLogin from "./hooks/useAutoLogin";
+import {useWeb3React} from "@web3-react/core";
+import useAuthorization from './hooks/useAuthorization';
 
 const getLibrary = (provider = null) => new providers.Web3Provider(provider);
 
 function App() {
   const isLoaded = useAutoLogin();
+  const { account: address } = useWeb3React();
+  const { loginMetamask, logout } = useAuthorization();
+
   return isLoaded && (
     <>
       <div className="page-wrapper">
         <img className="background" src={bg} alt="background"/>
+        <Menu address={address} login={loginMetamask} logout={logout}/>
         <div className="white-overlay" />
-        <Menu/>
         <div className="container">
           <Content/>
         </div>
