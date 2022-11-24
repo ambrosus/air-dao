e01bd1975ac9706d7fa86ec215a57fe59c2ad1cf
dimavoronin <zveruha1109>
2022-11-24 10:54:47 +0200

    added nofollow rel to links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-// import { useWeb3React } from '@web3-react/core';
-// import Layout from './components/Layout';
+import { useWeb3React } from '@web3-react/core';
+import Layout from './components/Layout';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-// import {
-//   useAuthorization,
-//   useAutoLogin,
-// } from 'airdao-components-and-tools/hooks';
+import {
+  useAuthorization,
+  useAutoLogin,
+} from 'airdao-components-and-tools/hooks';
 
 function App() {
-  // const web3React = useWeb3React();
-  // const isLoaded = useAutoLogin(web3React);
-  // const { loginMetamask, logout } = useAuthorization(web3React);
+  const web3React = useWeb3React();
+  const isLoaded = useAutoLogin(web3React);
+  const { loginMetamask, logout } = useAuthorization(web3React);
 
   return (
-    // isLoaded && (
-    // <Layout {...{ address: web3React.account, logout, login: loginMetamask }}>
-    <RouterProvider router={router} />
-    // </Layout>
-    // )
+    isLoaded && (
+      <Layout {...{ address: web3React.account, logout, login: loginMetamask }}>
+        <RouterProvider router={router} />
+      </Layout>
+    )
   );
 }
 
