902ec384753aa731134874fac0969824bcf6e26d
igorkishko <kishko.igor@gmail.com>
2022-11-30 10:48:03 +0200

    updated airdao-component-and-tools

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import { useWeb3React } from '@web3-react/core';
 import Layout from './components/Layout';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import {
-  useAuthorization,
-  useAutoLogin,
-} from 'airdao-components-and-tools/hooks';
+import { useAutoLogin } from 'airdao-components-and-tools/hooks';
 
 function App() {
-  const web3React = useWeb3React();
-  const isLoaded = useAutoLogin(web3React);
-  const { loginMetamask, logout } = useAuthorization(web3React);
+  const web3ReactInstance = useWeb3React();
+  const isLoaded = useAutoLogin(web3ReactInstance);
 
   return (
     isLoaded && (
-      <Layout {...{ address: web3React.account, logout, login: loginMetamask }}>
+      <Layout {...{ web3ReactInstance }}>
         <RouterProvider router={router} />
       </Layout>
     )
