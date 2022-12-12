7c24721a8806dca7bd7f70e074ad2f84351bd230
d.voronin <echelon1109@gmail.com>
2022-12-12 11:35:34 +0200

    fixed not found layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useWeb3React } from '@web3-react/core';
 import Layout from './components/Layout';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-// import { useAutoLogin } from 'airdao-components-and-tools/hooks';
+import { useAutoLogin } from 'airdao-components-and-tools/hooks';
 
 function App() {
   const web3ReactInstance = useWeb3React();
-  // const isLoaded = useAutoLogin(web3ReactInstance);
+  const isLoaded = useAutoLogin(web3ReactInstance);
 
   return (
-    true && (
+    isLoaded && (
       <Layout {...{ web3ReactInstance }}>
         <RouterProvider router={router} />
       </Layout>
