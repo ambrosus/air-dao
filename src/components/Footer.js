bc47f1639392ef0104ec14fb4a8d0f57c822e056
d.voronin <echelon1109@gmail.com>
2023-01-19 14:16:41 +0200

    changed year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     >
       <div className='container footer__wrapper'>
         <div className='footer__left'>
-          <p className='footer__grey footer__grey--margin'>©2022 AirDAO</p>
+          <p className='footer__grey footer__grey--margin'>©2023 AirDAO</p>
           <p className='footer__grey'>All rights reserved</p>
         </div>
       </div>
