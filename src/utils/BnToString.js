cc014704b57b43d67afe45714a9b160fa8d4b558
igorkishko <kishko.igor@gmail.com>
2023-04-12 21:50:10 +0300

    prettify price per one bond, restrict user to send less then 1 bond, clear inputs on successful swap

--- src/utils/BnToString.js
+++ src/utils/BnToString.js
@@ -1,10 +1,10 @@
 import { ethers } from 'ethers';
 
-const BnToString = (bn) => {
+const BnToString = (bn, decimalsLength = 3) => {
   const floatString = ethers.utils.formatEther(bn);
   const [intPart, floatPart] = floatString.split('.');
   if (floatPart && floatPart.length > 3) {
-    return `${intPart}.${floatPart.slice(0, 3)}`;
+    return `${intPart}.${floatPart.slice(0, decimalsLength)}`;
   } else if (floatPart && parseInt(floatPart) !== 0) {
     return `${intPart}.${floatPart}`;
   } else {
