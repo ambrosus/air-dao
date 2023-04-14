310af52594d966b32d9fa821cab65797e2c4bd6b
d.voronin <echelon1109@gmail.com>
2023-04-14 16:05:34 +0300

    added marketplace link

--- src/components/Claim/SuccessClaim.js
+++ src/components/Claim/SuccessClaim.js
@@ -7,16 +7,11 @@ const SuccessClaim = () => (
       Your BOND tokens are now in your wallet. You will be able to trade them on
       the AirBond Marketplace soon.
     </p>
-    <p className='claim-block__subtitle'>
-      Join the waitlist to get notified when the AirBond Marketplace launches!
-    </p>
     <UiButton
       withBorder
       className='claim-block__btn claim-block__btn_marketplace'
     >
-      <a href='/' target='_blank' rel='noreferrer'>
-        Join Waitlist
-      </a>
+      <a href='/bond-exchange/'>Bond Marketplace</a>
     </UiButton>
   </>
 );
