2c40d37ed1050046fb61c5994b916acaa582e1f6
igorkishko <kishko.igor@gmail.com>
2023-07-06 15:15:44 +0300

    use "provider" instead of "library" property from useWeb3React

--- src/pages/BondExchange/hooks/useBalances.js
+++ src/pages/BondExchange/hooks/useBalances.js
@@ -4,39 +4,39 @@ import { createAirBondContract } from '../../../contracts';
 import { useWeb3React } from '@web3-react/core';
 
 export default function useBalances() {
-  const { library, account } = useWeb3React();
+  const { provider, account } = useWeb3React();
   const [balances, setBalances] = useState({
     ambBalance: '',
     airBondBalance: '',
   });
 
-  const getAmbBalance = async (library, account) => {
-    const bnBalance = await library.getBalance(account);
+  const getAmbBalance = async (provider, account) => {
+    const bnBalance = await provider.getBalance(account);
     return BnToString(bnBalance);
   };
 
-  const getAirBondsBalance = async (library, account) => {
-    const AirBondsContract = await createAirBondContract(library);
+  const getAirBondsBalance = async (provider, account) => {
+    const AirBondsContract = await createAirBondContract(provider);
     const bnBalance = await AirBondsContract.balanceOf(account);
     return BnToString(bnBalance);
   };
 
   useEffect(() => {
-    if (!library || !account) {
+    if (!provider || !account) {
       setBalances({ ambBalance: '', airBondBalance: '' });
       return;
     }
 
     const getBalances = async () => {
-      const ambBalance = await getAmbBalance(library, account);
-      const airBondBalance = await getAirBondsBalance(library, account);
+      const ambBalance = await getAmbBalance(provider, account);
+      const airBondBalance = await getAirBondsBalance(provider, account);
       setBalances({ ambBalance, airBondBalance });
     };
 
     getBalances();
-    library.on('block', getBalances);
-    return () => library.off('block', getBalances);
-  }, [account, library]);
+    provider.on('block', getBalances);
+    return () => provider.off('block', getBalances);
+  }, [account, provider]);
 
   return balances;
 }
